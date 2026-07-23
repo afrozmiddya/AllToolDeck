ead15758431d47b43d8c970ea7091d032322230e
afrozmiddya <160333462+afrozmiddya@users.noreply.github.com>
2026-07-23 22:06:42 +0530

    Fix unused useState import in ToolLayout

--- apps/client/src/components/common/ToolLayout.tsx
+++ apps/client/src/components/common/ToolLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useLocation, Link } from "react-router-dom";
 import { toolsRegistry } from "@/data/toolsRegistry";
 import { SEO } from "./SEO";
